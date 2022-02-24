a50f31737e84af4da4a07f4b0cc425fff757c283
devjhl <jeonghyeon.dev@gmail.com>
2022-02-24 17:25:19 +0900

    権限

--- src/main/java/com/dev/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/dev/springsecurity/config/SecurityConfig.java
@@ -4,16 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.cos.blog.config.auth.PrincipalDetailService;
 import com.dev.springsecurity.auth.PrincipalDetailsService;
 
 @Configuration
 @EnableWebSecurity //Spring security filterがSpring filter chainに登録される
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true) //secured preAuthorize annotation 活性化
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
